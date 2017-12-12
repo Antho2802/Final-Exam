5b3ec1e13ebe3a272a6b2844e83fe47ea40df668
antho2802 <anthoboutin2802@hotmail.fr>
2017-12-12 13:41:54 +0100

    solution0)

--- spec/convert.spec.js
+++ spec/convert.spec.js
@@ -1,7 +1,7 @@
 describe('Test des parametres de la fonction', function() {
 	
 	it('la fonction doit prendre 3 parametres', function() {
-		expect(convertisseur('USD', 2, 'EUR')).not.toBe(null);
+		expect(convertisseur('USD', 10, 'EUR')).not.toBe(null);
     });
     
     it('les 3 parametres doivent etre du bon type', function() {
@@ -13,7 +13,7 @@ describe('Test des parametres de la fonction', function() {
 describe('Test resultats de la fonction', function() {
 	
 	it('doit convertir des USD dans les autres monnaies', function() {
-		
+    
     });
     
     it('doit convertir des EUR dans les autres monnaies', function() {
@@ -21,11 +21,15 @@ describe('Test resultats de la fonction', function() {
     });
 
     it('doit convertir des KRW dans les autres monnaies', function() {
-		
+		expect(convertisseur('KRW', 10, 'EUR')).not.toBe(null);
+        expect(convertisseur('KRW', 10, 'USD')).not.toBe(null);
+        expect(convertisseur('KRW', 10, 'NZD')).not.toBe(null);
     });
 
     it('doit convertir des NZD dans les autres monnaies', function() {
-		
+		expect(convertisseur('NZD', 10, 'EUR')).not.toBe(null);
+        expect(convertisseur('NZD', 10, 'USD')).not.toBe(null);
+        expect(convertisseur('NZD', 10, 'KRW')).not.toBe(null);
     });
 
     it('ne doit pas convertir des mauvais montants', function() {
